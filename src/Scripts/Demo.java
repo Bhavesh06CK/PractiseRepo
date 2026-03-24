ab869d8788a3994852ea4d4945e27652ff7776c0
QA_Lead <belliappa.bhavesh91@gmail.com>
2026-03-24 14:25:03 +0530

    Adding same scripts after modification

--- src/Scripts/Demo.java
+++ src/Scripts/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 
 	public static void main (String[]args) {
 		System.out.println("FirstClass for Git");
+		System.out.println("Checking_Push");
+		System.out.println("Add to Master");
 	} 
 }
